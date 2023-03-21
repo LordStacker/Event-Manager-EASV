452728ffea7cc6a3ebbf076c3ef15eec97f64cb2
Nicola Martinez <nicoenrique_1994@hotmail.com>
2023-03-21 22:30:46 +0100

    added TODO and some logic fields

--- src/main/java/dk/easv/gui/controllers/LoginController.java
+++ src/main/java/dk/easv/gui/controllers/LoginController.java
@@ -55,6 +55,7 @@ public class LoginController implements Initializable {
             stage.centerOnScreen();
             controller.initialed();
 
+
         }
 
     }
--- src/main/java/dk/easv/gui/controllers/MainWindowController.java
+++ src/main/java/dk/easv/gui/controllers/MainWindowController.java
@@ -148,6 +148,8 @@ public class MainWindowController implements Initializable {
             Parent parent = fxmlLoader.load();
             LoginController loginController = fxmlLoader.getController();
             loginController.setStage(stage);
+            //TODO clear all existing variables
+            //NICOLA
             stage.setScene(new Scene(parent));
         } catch (IOException e) {
             throw new RuntimeException(e);
