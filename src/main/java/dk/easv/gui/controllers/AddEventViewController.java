7d4930c64f9f0993bd10223e7e3b9338a9fe29f7
Nicola Martinez <nicoenrique_1994@hotmail.com>
2023-03-23 18:58:46 +0100

    merge alert merged with jakov

--- src/main/java/dk/easv/gui/controllers/AddEventViewController.java
+++ src/main/java/dk/easv/gui/controllers/AddEventViewController.java
@@ -71,6 +71,36 @@ public class AddEventViewController implements Initializable {
         this.stage = (Stage) submitButton.getScene().getWindow();
         stage.setMinWidth(600);
         stage.setMinHeight(450);
+
+    }
+
+    public void checkForInput(){
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(eventStartField.getText());
+        } catch (ParseException e) {
+            AlertHelper.showDefaultAlert("Please follow the format instructions when setting time of event!", Alert.AlertType.WARNING);
+        }
+
+        if (eventNameField == null ||eventNameField.getText().trim().isEmpty()){
+            AlertHelper.showDefaultAlert("Please enter event name before submitting!", Alert.AlertType.WARNING);
+        } else if (eventLocationField == null ||eventLocationField.getText().trim().isEmpty()) {
+            AlertHelper.showDefaultAlert("Please enter event location before submitting!", Alert.AlertType.WARNING);
+        } else if (startDatePicker.getValue() == null) {
+            AlertHelper.showDefaultAlert("Please enter event start date before submitting!", Alert.AlertType.WARNING);
+        } else if (endDatePicker.getValue() == null) {
+            AlertHelper.showDefaultAlert("Please enter event end date before submitting!", Alert.AlertType.WARNING);
+        } else if (eventStartField == null || eventStartField.getText().trim().isEmpty()) {
+            AlertHelper.showDefaultAlert("Please enter event start hour before submitting!", Alert.AlertType.WARNING);
+        } else if (eventEndField == null || eventEndField.getText().trim().isEmpty()) {
+            AlertHelper.showDefaultAlert("Please enter event end hour before submitting!", Alert.AlertType.WARNING);
+        } else if (eventDirectionsField == null || eventDirectionsField.getText().trim().isEmpty()) {
+            AlertHelper.showDefaultAlert("Please enter event directions before submitting!", Alert.AlertType.WARNING);
+        } else if (eventExtraNotesField == null || eventExtraNotesField.getText().trim().isEmpty()) {
+            AlertHelper.showDefaultAlert("Please enter extra notes about the event before submitting!", Alert.AlertType.WARNING);
+        }
     }
 
     @FXML
