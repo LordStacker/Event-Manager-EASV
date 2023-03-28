c0a566cedf3d7a6925d5ff901c323335a6ef6c33
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-03-28 20:47:53 +0200

    add check for config + move tmp folder creation

--- src/main/java/dk/easv/gui/controllers/MainWindowController.java
+++ src/main/java/dk/easv/gui/controllers/MainWindowController.java
@@ -23,6 +23,8 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.ResourceBundle;
@@ -54,6 +56,16 @@ public class MainWindowController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setTables();
+
+        // check for tmp folder
+        Path path = Path.of("src/main/resources/dk/easv/tmp");
+        if (!Files.exists(path)){
+            try {
+                Files.createDirectory(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     private void setTables() {
